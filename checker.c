643db2d049f35e547dfdd4a0bac6f1d1d508b5b8
RakshithR98 <rakshithraghu2017@gmail.com>
2024-08-30 09:31:39 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
 #include <assert.h>
 
-int isTemperatureOk(float temperature) 
-{
-  if(temperature < 0 || temperature > 45) 
-  {
-    printf("Temperature out of range!\n");
-    return 0;
-  }
-  return 1;
+typedef struct {
+    float value;
+    float min;
+    float max;
+    char errorMessage[50];  
+} ParameterCheck;
+
+int isParameterOk(ParameterCheck check) {
+    if (check.value < check.min || check.value > check.max) {
+        printf("%s\n", check.errorMessage);
+        return 0;
+    }
+    return 1;
 }
 
-int isSocOk(float soc) 
-{
-  if(soc < 20 || soc > 80) 
-  {
-    printf("State of Charge out of range!\n");
-    return 0;
-  }
-  return 1;
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+   
+    ParameterCheck checks[3] = {
+        {temperature, 0, 45, "Temperature out of range!"},
+        {soc, 20, 80, "State of Charge out of range!"},
+        {chargeRate, 0, 0.8, "Charge Rate out of range!"}
+    };
+
+    for (int i = 0; i < 3; i++) {
+        if (!isParameterOk(checks[i])) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
-int isChargeRateOk(float chargeRate) 
-{
-  if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
-}
-
-int batteryIsOk(float temperature, float soc, float chargeRate) 
-{
-  return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
-}
-
-int main() 
-{
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+int main() {
+    assert(batteryIsOk(25, 70, 0.7)); 
+    assert(!batteryIsOk(50, 85, 0));  
 }
